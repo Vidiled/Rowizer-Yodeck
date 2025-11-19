a9033691450e1c4ba86b9eded6823c1f2a41ff3c
Bart Smits <bart@seza.si>
2025-11-19 14:18:19 +0100

    Test with simpler date

--- js/script.js
+++ js/script.js
@@ -203,6 +203,7 @@ function initApp(config){
 // Yodeck will call init_widget(config) with a JS object containing the configuration
 // See Yodeck docs: the player calls this function after loading the app
 window.init_widget = function(config){
+    console.log('init_widget called with config:', config);
     try{
         initApp(config);
     }catch(e){
